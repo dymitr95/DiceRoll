aef5502ce798343f0fdf587cf305af16cdc0f87d
Dymitr Dragalow <d.dragalow@gmail.com>
2025-08-03 21:06:41 +0200

    Add swagger

--- DiceRollBackend/DiceRollBackend.API/Program.cs
+++ DiceRollBackend/DiceRollBackend.API/Program.cs
@@ -12,6 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -31,6 +35,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
